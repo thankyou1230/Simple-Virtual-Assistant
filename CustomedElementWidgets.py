561e0bffb6c1a94edf12cc1daea0093b04ffb696
Huynh Quang <quang0002@gmail.com>
2021-05-11 12:13:31 +0700

    UI

---
+++ CustomedElementWidgets.py
@@ -0,0 +1,100 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from MessageBubble import MessageDelegate, MessageModel
+
+class RecordButton(QtWidgets.QPushButton):
+    def __init__(self, *args, **kwargs):
+        super(RecordButton, self).__init__(*args, **kwargs)
+        self.setGeometry(QtCore.QRect(10, 460, 41, 41))
+        self.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.setMouseTracking(True)
+        self.setToolTipDuration(1500)
+        self.setStyleSheet("QPushButton{\n"
+                                        "background: none;\n"
+                                        "border:none;\n"
+                                        "border-radius:20%\n"
+                                        "}\n"
+                                        "QPushButton:hover{\n"
+                                        "background:rgb(166, 200, 255)\n"
+                                        "}")
+        self.setText("")
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("./Icon/recordVoice.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.setIcon(icon)
+        self.setIconSize(QtCore.QSize(40, 40))
+        self.setObjectName("recordButton")
+
+###################################################################
+###################################################################
+
+class SendButton(QtWidgets.QPushButton):
+    def __init__(self, *args, **kwargs):
+        super(SendButton, self).__init__(*args, **kwargs)
+        self.setGeometry(QtCore.QRect(380, 460, 41, 41))
+        self.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.setMouseTracking(True)
+        self.setToolTipDuration(1500)
+        self.setStyleSheet("QPushButton{\n"
+                                      "background: none;\n"
+                                      "border:none;\n"
+                                      "border-radius:20%\n"
+                                      "}\n"
+                                      "QPushButton:Hover{\n"
+                                      "background: rgb(160, 196, 255);\n"
+                                      "}")
+        self.setText("")
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("./Icon/send.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.setIcon(icon)
+        self.setIconSize(QtCore.QSize(55, 53))
+        self.setObjectName("sendButton")
+
+######################################################################
+######################################################################
+
+class MessageEditor(QtWidgets.QTextEdit):
+    def __init__(self, *args, **kwargs):
+        super(MessageEditor, self).__init__(*args, **kwargs)
+        self.setGeometry(QtCore.QRect(60, 460, 311, 41))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(10)
+        self.setFont(font)
+        self.viewport().setProperty("cursor", QtGui.QCursor(QtCore.Qt.IBeamCursor))
+        self.setStyleSheet("QTextEdit{\n"
+                                   "border: none;\n"
+                                   "border-radius: 20px;\n"
+                                   "corlor: rgb(209, 209, 209);\n"
+                                   "background: rgb(208, 208, 208);\n"
+                                   "padding-top:7%;\n"
+                                   "padding-left:10px;\n"
+                                   "font-size:10pt;\n"
+                                   "padding-right:15px;\n"
+                                   "padding-bottom:6%\n"
+                                   "}")
+        self.setLocale(QtCore.QLocale(QtCore.QLocale.Vietnamese, QtCore.QLocale.Vietnam))
+        self.setLineWidth(2)
+        self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.setPlaceholderText("Type here !")
+        self.setObjectName("chatBox")
+    def keyPressEvent(self, e):
+        if e.key()
+
+##########################################################################
+##########################################################################
+
+class MessageView(QtWidgets.QListView):
+    def __init__(self, *args, **kwargs):
+        super(MessageView, self).__init__(*args, *kwargs)
+        self.setGeometry(QtCore.QRect(10, 10, 410, 430))
+        self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded)
+        self.setAutoFillBackground(True)
+        self.setStyleSheet("QListView{\n"
+                                      "border:none;\n"
+                                      "background:white;\n"
+                                      "}")
+        self.setObjectName("MessageView")
+        self.setItemDelegate(MessageDelegate())
+        self.MessageList = MessageModel()
+        self.setModel(self.MessageList)
+        self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+
---
+++ MessageBubble.py
@@ -0,0 +1,56 @@
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+# from PyQt5.QtGui import
+
+PADDING = QtCore.QMargins(10,10,110,20)
+BACK_PADDING = QtCore.QMargins(0,0,100,10)
+FONT=QtGui.QFont('Arial',10)
+USER_TEXT=QtGui.QColor('white')
+USER_BACK=QtGui.QColor('#217eff')
+BOT_TEXT=QtGui.QColor('black')
+BOT_BACK=QtGui.QColor('#c8d0e3')
+
+class MessageDelegate(QtWidgets.QStyledItemDelegate):
+    def paint(self, painter, option, index):
+        sender_name, Text=index.model().data(index, QtCore.Qt.DisplayRole)
+        painter.setFont(QtGui.QFont(FONT))
+        painter.setPen(QtCore.Qt.NoPen)
+        if sender_name=='user':
+            painter.setBrush(USER_BACK)
+        elif sender_name=='bot':
+            painter.setBrush(BOT_BACK)
+        textrect=option.rect.marginsRemoved(PADDING)
+        backrect=option.rect.marginsRemoved(BACK_PADDING)
+        painter.drawRect(backrect)
+        if sender_name=='user':
+            painter.setPen(USER_TEXT)
+        elif sender_name=='bot':
+            painter.setPen(BOT_TEXT)
+        painter.drawText(textrect,QtCore.Qt.TextWordWrap,Text,)
+    def sizeHint(self, option, index):
+        _, text = index.model().data(index, QtCore.Qt.DisplayRole)
+        metrics = QtGui.QFontMetrics(FONT)
+        rect = option.rect
+        rect = metrics.boundingRect(rect, QtCore.Qt.TextWordWrap, text)
+        rect = rect.marginsAdded(PADDING)
+        return rect.size()
+
+
+##################################################################
+
+class MessageModel(QtCore.QAbstractListModel):
+    def __init__(self, *args, **kwargs):
+        super(MessageModel, self).__init__(*args, **kwargs)
+        self.Messages=[]
+    def data(self, index, role):
+        if (role == QtCore.Qt.DisplayRole):
+            return self.Messages[index.row()]
+    def rowCount(self, index):
+        return len(self.Messages)
+    def addMessage(self, sender_name, message):
+        if message:
+            self.Messages.append((sender_name, message))
+            self.layoutChanged.emit()
+    def count(self):
+        return len(self.Messages)
+ 
