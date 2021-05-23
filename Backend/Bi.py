adba74e775fc18511586683f24360f1648b98888
Huynh Quang <quang0002@gmail.com>
2021-05-24 01:50:44 +0700

    Link module

--- Backend/Bi.py
+++ Backend/Bi.py
@@ -1,6 +1,4 @@
-from gtts import gTTS
 import os
-import playsound
 import speech_recognition as sr
 import smtplib
 from email.mime.text import MIMEText
@@ -30,6 +28,7 @@ class Bi():
     def speak(self,text):
         self.mouth.say(text)
         self.mouth.runAndWait()
+        return text
 
     ##########################  2
     def listen(self):
@@ -40,7 +39,7 @@ class Bi():
                 text = self.ear.recognize_google(audio, language="vi-VN")
                 return text
             except Exception as e:
-                return False
+                return ''
    
     ##########################  3
     def send_email(self,destination,subject,content):
@@ -56,16 +55,15 @@ class Bi():
                 mail.login('quang0002@gmail.com', '0917787421q')
                 mail.sendmail('quang0002@gmail.com',destination, msg.as_string().encode('utf-8'))
                 mail.close()
-            self.speak('Tôi đã gửi đi thành công rồi nhé')
-            return True
+            return self.speak('Tôi đã gửi đi thành công rồi nhé')
         except Exception as e:
-            self.speak('Xin lỗi hiện tại không gửi được rồi, hãy thử lại sau nhé')
+            return self.speak('Xin lỗi hiện tại không gửi được rồi, hãy thử lại sau nhé')
     
     ########################### 4
     def change_random_walpaper(self):
         api_key = 'lTRwvyGb1vAmzbDCruvqzCR7hdFpa4HfG-gjNZkDbkc'
         url = 'https://api.unsplash.com/photos/random?client_id=' + api_key 
-        self.speak('Bạn chờ một chút nhé')
+        self.speak('Ô kê, mất vài giây thôi')
         try:   
             f = urllib.request.urlopen(url)
             json_string = f.read()
@@ -76,23 +74,21 @@ class Bi():
             save_path=os.path.join(pathlib.Path(__file__).parent.absolute(),'Source\img.png')
             urllib.request.urlretrieve(photo, save_path)
             ctypes.windll.user32.SystemParametersInfoW(20,0,save_path,3)
-            self.speak('Tôi đã đổi xong rồi đấy, bạn xem thử xem')
-            return True
+            return self.speak('Mình đã đổi xong rồi đấy, bạn coi thử xem')
         except Exception as e:
-            self.speak('Xin lỗi hiện tại tôi không thể đổi được ảnh nền, xin hãy thử lại sau')
-            return False
+            return self.speak('Xin lỗi hiện tại mình không thể đổi được ảnh nền giúp bạn, một lát thử lại nhé')
+
     
     ############################# 5
     def search_google(self,serch_text):
         url="https://www.google.com.tr/search?q={}".format(serch_text)
-        self.speak('Có ngay đây')
+        self.speak('Đợi chút nha')
         try:
             webbrowser.register('chrome',None,webbrowser.BackgroundBrowser("C:\Program Files\Google\Chrome\Application\chrome.exe"))
             webbrowser.get('chrome').open_new_tab(url)
-            return True
+            return self.speak('Có rồi nhé')
         except Exception as e:
-            self.speak('Xin lỗi, hiện tại tôi không thể giúp bạn tìm kiếm. Xin thử lại sau')
-            return False
+            return self.speak('Xin lỗi, hiện tại mình không gọi được cho chị gu gồ')
     
     ############################# 6
     def wheather_forecast(self):
@@ -115,14 +111,13 @@ class Bi():
             sunrise=datetime.datetime.fromtimestamp(data['sys']['sunrise'])
             sunset=datetime.datetime.fromtimestamp(data['sys']['sunset'])
             #respone
-            forecast_script='Đây rồi. Thời tiết {} hôm nay có {}, nhiệt độ trung bình là {} độ C, độ ẩm là {}%, sức gió {} km trên giờ,\
+            self.speak('đây rồi')
+            forecast_script='Thời tiết {} hôm nay có {}, nhiệt độ trung bình là {} độ C, độ ẩm là {}%, sức gió {} km trên giờ,\
                         tầm nhìn xa trên {} mét. Hôm nay mặt trời mọc lúc {} giờ {} phút, lặn lúc {} giờ {} phút. Xin hết'.\
                         format(city,weather,temp,humid,wind,vision,sunrise.hour,sunrise.minute,sunset.hour,sunset.minute)
-            self.speak(forecast_script)
-            return True
+            return self.speak(forecast_script)
         except Exception as e:
-            self.speak('Xin lỗi hiện tại tôi không thể truy cập vào dữ liệu thời tiết. Xin thử lại sau')
-            return False
+            return self.speak('Bạn ơi hiện tại mình không thể truy cập vào dữ liệu thời tiết, xin lỗi nhé')
 
     ############################## 7
     def play_youtube(self,video_name):
@@ -131,29 +126,70 @@ class Bi():
             video = youtube_search.YoutubeSearch(video_name, max_results=1).to_dict()
             webbrowser.register('chrome',None,webbrowser.BackgroundBrowser("C:\Program Files\Google\Chrome\Application\chrome.exe"))
             webbrowser.get('chrome').open_new_tab('https://www.youtube.com/watch?v='+video[0]['id'])
-            self.speak('Tôi mở rồi đấy nhé')
-            return True
+            return self.speak('Tôi mở rồi đấy. Mời bạn thưởng thức nhá')
         except Exception as e:
-            self.speak('Xin lỗi, hiện tại tôi không thể làm được. Xin thử lại sau')
-            return False
+            return self.speak("Hình như hết tiền mạng rồi :D")
     ############################## 8
     def define(self,name):
-        self.speak('Để tôi nhớ xem')
-        wikipedia.set_lang('vi')
+        self.speak('Để mình nhớ xem')
         try:
+            wikipedia.set_lang('vi')
             contents = wikipedia.summary(name).split('.')
-            self.speak('Theo tôi biết. ' +contents[0])
-            return True
+            self.speak('Theo mình biết: ' +contents[0])
         except Exception as e:
-            self.speak('Tôi cũng không biết nữa, tìm trên google xem sao')
-            return self.search_google(name)
+            return self.speak('Mình cũng không biết nữa. Chị gu gồ chắc sẽ biết đó')
+    
     ############################# 9
-    def set_alarm(self,time):
-        pass
+    def get_time(self):
+        t = time.localtime()
+        return time.strftime('%H:%M:%S', t)
+
+    def get_date(self):
+        t = time.localtime()
+        return time.strftime('%d/%m/%Y', t)
     
     ############################ 10
     def analysis_request(self, request):
-        pass
+        request=request.lower()
+        if not request:
+            pass
+        elif "dừng" in request or "tạm biệt" in request:
+            pass
+        elif "hiện tại" in request or "mấy giờ" in request or 'bây giờ là' in request:
+            response='Bây giờ là '+self.get_time
+            self.speak(response)
+            return response
+        elif 'hôm nay là' in request or 'ngày mấy' in request:
+            response='Hôm nay là '+self.get_date
+            self.speak(response)
+            return response
+        elif 'tìm' in request:
+            return self.search_google(request.split(" tìm ",1)[1])
+        elif 'mở' in request or 'phát' in request:
+            return self.play_youtube(request)
+        elif 'gửi mail' in request or 'gởi mail' in request:
+            self.speak('Bạn muốn gửi mail cho ai')
+            destination=self.listen()
+            self.speak('Bạn muốn đặt tiêu đề là gì')
+            subject=self.listen()
+            self.speak('Nội dung của mail là gì')
+            content=self.listen()
+            return self.send_email(destination,subject,content)
+        elif 'đổi ảnh' in request or 'thay ảnh' in request or 'đổi hình' in request or 'thay hình' in request or 'đổi nền' in request or 'thay nền' in request:
+            return self.change_random_walpaper()
+        elif 'thời tiết' in request:
+            return self.wheather_forecast()
+        elif 'là gì' in request or 'là ai' in request:
+            self.define(request.replace('là gì','').replace('là ai',''))
+        else:
+            pass
+            #chatbot respone
+
+
+
+
+
+        
             
 
     ############################# 11
@@ -168,9 +204,4 @@ class Bi():
 
 if __name__=='__main__':
     bot=Bi()
-    while 1:
-        a=bot.listen()
-        print(a)
-        if a:    
-            bot.define(a)
-        time.sleep(2)
+    bot.analysis_request('Đổi ảnh nền giúp tôi nhé')
